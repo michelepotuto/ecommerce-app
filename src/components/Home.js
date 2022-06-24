a4b1fe968b13fb702f245662d9936164f549a56e
michelepotuto <michele.potuto1990@gmail.com>
2022-06-25 01:39:23 +0200

    commitDiMezzanotte3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import logo from '../assets/Apple_iphone.jpg';
+import logo1 from '../assets/Apple_iphone.jpg';
+import logo2 from '../assets/Asus_rog.jpg';
+import logo3 from '../assets/Sedia_gaming.jpg';
 
 const Home = () => (
   <div className="h1"> <hr></hr> ⮊ BENVENUTO IN TEAM SHOP! ⮈<hr></hr>
 <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={logo} className="d-block w-100" alt="..."/>
+      <img src={logo1} className="d-block w-100" alt="logo1"/>
     </div>
     <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src={logo2} className="d-block w-100" alt="logo2"/>
     </div>
     <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
+      <img src={logo3} className="d-block w-100" alt="logo3"/>
     </div>
   </div>
 </div>
