73a4c0fc6b6aa72ecdfae718a74372627389b83b
ElleR01 <lollo4216@gmail.com>
2022-06-25 10:06:52 +0200

    test carrello

--- src/components/ProdottoSingolo.js
+++ src/components/ProdottoSingolo.js
@@ -1,52 +1,47 @@
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
-import useCart from "../hooks/use-cart";
-
+/* import useCart from "../hooks/use-cart"; */
 const Singolo = (prop) => {
   const { nome, categoria, descrizioneB, descrizioneD, img, quantità, prezzo } =
     prop.prodotto;
-  const { addToCart } = useCart();
+    
+  /* const { addToCart } = useCart(); */
   return (
     <div>
       <div>
         <div className="card">
-          <div class="row align-items-center">
-            <div class="col">
+          <div className="row align-items-center">
+            <div className="col-md-4 float-start">
               <img src={`${img}`} className="img-thumbnail" alt="..." />
             </div>
-            <div class="col">
+            <div className="col">
               <div className="card-body">
                 <h5 className="card-title">
                   {nome} - {categoria}
                 </h5>
                 <p className="card-text">{descrizioneB}</p>
-                <p className="Prezzo">{prezzo}</p>
-                <Button
+                <p className="Prezzo">{prezzo} € </p>
+                <div className="card text-end">
+                <Link to="/">Dettagli</Link>
+              </div>
+              </div>
+            </div>
+            <div className="col"><Button
                   onClick={() => {
-                    addToCart(prop.prodotto);
+                   prop.addToCart(prop.prodotto);
+                   console.log("Singolo add : " + prop.prodotto);
+                   /*  addToCart(prop.prodotto); */
                   }}
                 >
                   Add to cart
                 </Button>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card text-end">
-                <Link to="/">Dettagli</Link>
-              </div>
+              
             </div>
           </div>
         </div>
       </div>
 
-      {/*  {nome} <br></br>
-        {categoria} <br></br>
-        {descrizioneB}<br></br>
-        {descrizioneD}<br></br>
-        {quantità}<br></br>
-        <img src={img}></img>
-
-        <hr></hr> */}
+     
     </div>
   );
 };
