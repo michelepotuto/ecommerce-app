18e5093747ceb743ac29889edfdc878cd2b37d77
ElleR01 <lollo4216@gmail.com>
2022-06-27 09:11:27 +0200

    merge issue cart

--- src/components/ProdottoSingolo.js
+++ src/components/ProdottoSingolo.js
@@ -9,74 +9,74 @@ const Singolo = (prop) => {
 
   const addToCartHandler = (product) => {
     console.log("aggiunto al localStorage:" + product);
-    if(!localStorage.getItem(storageKey2)){
+    if (!localStorage.getItem(storageKey2)) {
 /*       console.log("COUNT " + localStorage.getItem(storageKey));
  */      localStorage.setItem(storageKey2, 1);
-    }else{
+    } else {
       const prev = parseInt(localStorage.getItem(storageKey2)) + 1;
       localStorage.setItem(storageKey2, prev);
     }
 
-    if(!localStorage.getItem(storageKey)){
+    if (!localStorage.getItem(storageKey)) {
       localStorage.setItem(storageKey, product);
-    }else{
-     const storageCart = [localStorage.getItem(storageKey),product];
-    localStorage.setItem(storageKey, storageCart);
+    } else {
+      const storageCart = [localStorage.getItem(storageKey), product];
+      localStorage.setItem(storageKey, storageCart);
     }
-    
+
 /*     console.log("aggiunto al localStorage:" + JSON.stringify(product));
  */  };
 
-  const {id, nome, categoria, descrizioneB, descrizioneD, img, quantità, prezzo } =
+  const { id, nome, categoria, descrizioneB, descrizioneD, img, quantità, prezzo } =
     prop.prodotto;
-    
-  const { addToCart } = useCart(); 
+
+  const { addToCart } = useCart();
   return (
-      <div className="container">
-        <div className="card">
-          <div className="row align-items-center">
-            <div className="col-md-4 float-start">
-              <img src={`${img}`} className="img-thumbnail" alt="..." />
+    <div className="container">
+      <div className="card">
+        <div className="row align-items-center">
+          <div className="col-md-4 float-start">
+            <img src={`${img}`} className="img-thumbnail" alt="..." />
+          </div>
+          <div className="col">
+            <div className="card-body">
+              <h5 className="card-title">
+                {nome} - {categoria}
+              </h5>
+              <p className="card-text">{descrizioneB}</p>
+              <p className="Prezzo">{prezzo} € </p>
             </div>
-            <div className="col">
-              <div className="card-body">
-                <h5 className="card-title">
-                  {nome} - {categoria}
-                </h5>
-                <p className="card-text">{descrizioneB}</p>
-                <p className="Prezzo">{prezzo} € </p>
-              </div>
-            </div>
-            <div className="col"><Button
-                  onClick={() => {
-                    addToCart(prop.prodotto);
-                    addToCartHandler(new Prodotto(id, nome, categoria, descrizioneB, descrizioneD, img, quantità, prezzo));
-                   console.log("Singolo add : " + prop.prodotto);
-                   /*  addToCart(prop.prodotto); */
-                  }}
-                >
-                  Add to cart
-                </Button>
-              </div>
-            </div>
-            <div class="col">
-              <div className="card text-end">
-              <NavLink ClassName="card text-end" to="/ProdottoDettagliato">
+          </div>
+          <div className="col"><Button
+            onClick={() => {
+              addToCart(prop.prodotto);
+              addToCartHandler(new Prodotto(id, nome, categoria, descrizioneB, descrizioneD, img, quantità, prezzo));
+              console.log("Singolo add : " + prop.prodotto);
+              /*  addToCart(prop.prodotto); */
+            }}
+          >
+            Add to cart
+          </Button>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card text-end">
+            <NavLink ClassName="card text-end" to="/ProdottoDettagliato">
               Dettagli
             </NavLink>
-            
-             
-            
-              <div className="card_text-end">
-                <NavLink to="/">Vai ai dettagli ⇢</NavLink>
-              </div>
-            
-            
+
+
+
+            <div className="card_text-end">
+              <NavLink to="/">Vai ai dettagli ⇢</NavLink>
+            </div>
+
+
           </div>
-          </div>
-     
-     
-    
+        </div>
+
+      </div>
+    </div>
   );
 };
 
