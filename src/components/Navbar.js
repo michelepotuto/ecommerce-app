bbde0a58ecd45257181ac2bfd7800c28893965d8
michelepotuto <michele.potuto1990@gmail.com>
2022-06-28 12:45:37 +0200

    ddd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import useCart from "../hooks/use-cart";
-import { Dropdown } from "react-bootstrap";
-
-const Navbar = () => {
-  const [cartCount, setCartCount] = useState(0);
-  const storageKey2 = "user-cart-count";
-
-  useEffect(() => {
-    if (localStorage.getItem(storageKey2)) {
-      setCartCount(parseInt(localStorage.getItem(storageKey2)));
-    }
-  }, []);
-
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="navbar-brand">☫ TEAM SHOP</div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <NavLink className="nav-link" to="/home">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/prodotti">
-              Prodotti
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Logout
-            </NavLink>
-          </li>
-
-          <li className="nav-item">
-          <NavLink to="/carrello">
-              Carrello ({cartCount})
-              </NavLink>
-              </li>
-
-        </ul>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
-=======
 import React, { useState, useEffect } from 'react';
 import {  useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
@@ -140,4 +77,3 @@ const Navbar = () => {
 
 
 export default Navbar
->>>>>>> 7b2422ff07434135ffa7b8c670d9013d54e3bdbf
