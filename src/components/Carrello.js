0ec6beec282e498f6d5e8bcfc19ca0f3c135b536
michelepotuto <michele.potuto1990@gmail.com>
2022-06-28 13:35:48 +0200

    css

--- src/components/Carrello.js
+++ src/components/Carrello.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ProdottoCart from './ProdottoCart';
 import { storageName } from "../store/counter-store";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Carrello = () => {
   const [length, setLength] = useState(0);
@@ -21,8 +21,8 @@ const Carrello = () => {
 
   return (
     <>
-      {length === 0 ? <div><h1 className='text-center'>Carrello vuoto</h1> <div><h2 className='text-center'><Link className="nav-link border border-1" to="/prodotti">
-        Inizia la tua spesa</Link></h2></div></div> :
+      {length === 0 ? <div><h1 className='text-center'>Carrello vuoto</h1> <div><h2 className='text-center'><button className="inizia_spesa border border-1" to="/prodotti">
+        <Link to='/prodotti'>Inizia la tua spesa</Link></button></h2></div></div> :
         storageCart.map((prodotto, key) => (
           <ProdottoCart key={key} prodotto={prodotto} />
         ))
